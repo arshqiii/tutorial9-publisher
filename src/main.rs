013ef9380973eb5f1cc770d5ed2e6adccd9a02f3
arshqiii <marshq23@gmail.com>
2025-05-14 09:56:55 +0700

    renamed npm for correction

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,13 @@ pub struct UserCreatedHandler;
 fn main() {
     let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "1".to_owned(), user_name: "129500004y-Amir".to_owned() });
+        user_id: "1".to_owned(), user_name: "2306275885y-Amir".to_owned() });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-        user_id: "2".to_owned(), user_name: "129500004y-Budi".to_owned() });
+        user_id: "2".to_owned(), user_name: "2306275885y-Budi".to_owned() });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-    user_id: "3".to_owned(), user_name: "129500004y-Cica".to_owned() });
+    user_id: "3".to_owned(), user_name: "2306275885y-Cica".to_owned() });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-    user_id: "4".to_owned(), user_name: "129500004y-Dira".to_owned() });
+    user_id: "4".to_owned(), user_name: "2306275885y-Dira".to_owned() });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
-    user_id: "5".to_owned(), user_name: "129500004y-Emir".to_owned() });
+    user_id: "5".to_owned(), user_name: "2306275885y-Emir".to_owned() });
 }
